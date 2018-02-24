66d6488703aa729aa38fa8b791de6f7e99aab840
zhangzhenwei <924909997@qq.com>
2018-02-24 17:22:47 +0800

    更改

--- myPhoto/ImagePicker/Cells/pickerCells/WWSelecteCVCell.swift
+++ myPhoto/ImagePicker/Cells/pickerCells/WWSelecteCVCell.swift
@@ -1,22 +1,9 @@
 //
 //  WWSelecteCVCell.swift
-//  WWImagePickrController-swift
+//  myPhoto
 //
-// **************************************************
-// *                                  _____         *
-// *         __  _  __     ___        \   /         *
-// *         \ \/ \/ /    / __\       /  /          *
-// *          \  _  /    | (__       /  /           *
-// *           \/ \/      \___/     /  /__          *
-// *                               /_____/          *
-// *                                                *
-// **************************************************
-//  Github  :https://github.com/631106979
-//  HomePage:http://imWW.com
-//  CSDN    :http://blog.csdn.net/wang631106979
-//
-//  Created by 王崇磊 on 16/9/14.
-//  Copyright © 2016年 王崇磊. All rights reserved.
+//  Created by zhangzhenwei on 2018/2/23.
+//  Copyright © 2018年 zhangzhenwei. All rights reserved.
 //
 // @class WWSelecteCVCell
 // @abstract WWSelecteView的cell
